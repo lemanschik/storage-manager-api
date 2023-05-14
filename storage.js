bad7551ca82bc800184ccb0202c385b5a610e6c4
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-14 13:08:00 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -9,17 +9,19 @@
 // FileSystemAccess Cache Default Manual Managed works most best in
 // most cases as no additional handling is needed.
 // path equals to the path that produced the handle without the name. 
-// resolvedPath = handle.path +'/' + handle.name `${handle.path}/${handle.name}`
+// resolvedPath = handle.path +'/' + handle.name `${handle.path||''}${handle.name ? '/':'.'}${handle.name||''}`
 // Why? to speed up symlinks and other processes. like a readdir followed by some fileOperations.
 const FileSystemIndex = {}
 // .entries .values .keys 
 // Exists as long as a dir is open when .close the FileSystemIndex gets Empty
 // Stores the current handle and its state if it is open or ReadWrite.
 
+
+// TODO: do not go relativ more deep in the fs module then current process.pwd and minimum deep process.pwd
 // if you do a readdir you get the dirEntrie, fileSystemIndexKey === path
-const IDirectoryHandle = (name,path) => ({
+const IDirectoryHandle = (name='',path='') => ({
   getDirectory: () => fs.promises
-    .readdir('.',{ recursive:false,withFileTypes: true }).then(dirEnts=>dirEnts.map(([ent,typeSymbol])=>IDirectoryHandle)),
+    .readdir(`${path}${ (path||name) ? '/' : '.' }${name}`,{ recursive:false,withFileTypes: true }).then(dirEnts=>dirEnts.map(([ent,typeSymbol])=>IDirectoryHandle)),
   name: dirEnt[0],
   kind: 'directory'
 });
@@ -28,8 +30,8 @@ const IFileHandle = (dirEnt,path) => ({
   getFile() {}
 })
 
-const IFileSystemHandle = (dirEnt) => dirEnt ? ({
-  ...([,'file','directory','link'].find((kind,i) => i === dirEnt[dirEnt[1]]) === 'directory' && IDirectoryHandle(dirEnt[0]))
+const IFileSystemHandle = (dirEnt,resolvedPath) => dirEnt ? ({
+  ...([,'file','directory','link'].find((kind,i) => i === dirEnt[dirEnt[1]]) === 'directory' && IDirectoryHandle(dirEnt[0],resolvedPath))
 }) : IDirectoryHandle('.')
 
 export StorageManager = navigator ? navigator.storage : IDirectoryHandle('.')
