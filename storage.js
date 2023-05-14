730aec3f43c2d526ddde4eead3b86ad718bf9532
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-14 12:39:51 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -2,7 +2,23 @@
 // if global then we use the users home 
 // To Apply System Changes we will follow the postfix pattern check fix for
 // files outside /root /home/user
+// path equals to the path that produced the handle without the name. 
+// resolvedPath = handle.path +'/' + handle.name
+// otherHandle.resolve(): runs find recursiv. 
+const IDirectoryHandle = (name,path) => ({
+  getDirectory() {}
+  name: dirEnt[0],
+  kind: 'directory'
+});
 
+const IFileHandle = (dirEnt) => ({
+  getFile() {}
+})
+
+const IFileSystemHandle = (dirEnt) => dirEnt ? ({
+  ...([,'file','directory','link'].find((kind,i) => i === dirEnt[dirEnt[1]]) === 'directory' && IDirectoryHandle(dirEnt[0]))
+}) : IDirectoryHandle('.')
 export StorageManager = navigator ? navigator.storage : ({
-  getDirectory: () => 
+  getDirectory: () => fs.promises
+    .readdir('.',{ recursive:false,withFileTypes: true }).then(dirEnts=>dirEnts.map(([ent,typeSymbol])=>IDirectoryHandle))
 })
