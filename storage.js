362df7c2ea0b7e144387e5287f3b3b646375f888
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-14 12:05:23 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,3 +1,8 @@
+// we should allways use the process.pwd as opfs location like npm does 
+// if global then we use the users home 
+// To Apply System Changes we will follow the postfix pattern check fix for
+// files outside /root /home/user
+
 export StorageManager = navigator ? navigator.storage : ({
   getDirectory: () => 
 })
