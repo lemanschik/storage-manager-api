27c05e75c4b07f8fe5f60ccea2314f6fbbfa5152
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-18 19:07:30 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,48 +1,164 @@
-// we should allways use the process.pwd as opfs location like npm does 
-// if global then we use the users home 
-// To Apply System Changes we will follow the postfix pattern check fix for
-// files outside /root /home/user
+import {ReadableStream} from 'node:stream';
 
-// otherHandle.resolve(): runs find recursiv. 
-
-
-// FileSystemAccess Cache Default Manual Managed works most best in
-// most cases as no additional handling is needed.
-// path equals to the path that produced the handle without the name. 
-// resolvedPath = handle.path +'/' + handle.name `${handle.path||''}${handle.name ? '/':'.'}${handle.name||''}`
-// Why? to speed up symlinks and other processes. like a readdir followed by some fileOperations.
-const FileSystemIndex = {}
-// .entries .values .keys 
-// Exists as long as a dir is open when .close the FileSystemIndex gets Empty
-// Stores the current handle and its state if it is open or ReadWrite.
-
-const getDirectory = (name,path) => () => fs.promises
-    .readdir(`${path}${ (path||name) ? '/' : '.' }${name}`,{ recursive:false,withFileTypes: true })
-    .then(dirEnts=>dirEnts.map((dirEnt) => IFileSystemHandle(dirEnt,path))
-
-// TODO: do not go relativ more deep in the fs module then current process.pwd and minimum deep process.pwd
-// if you do a readdir you get the dirEntrie, fileSystemIndexKey === path
-const IDirectoryHandle = (name='',path='') => ({
-  resolve: async (handle) => getDirectory(name,path)
-  getDirectory: getDirectory(name,path),
-  name,kind: 'directory'
-});
-
-const IFileHandle = (dirEnt,path) => ({
-  getFile() {}
+const opendir = getDirectory().values
+const openDirStream = (directory: string, { 
+  recursive: false, withFileTypes: true } = {}
+) => new ReadableStream({        
+  async start(controller) { 
+    for await (const file of await opendir(directory)) {
+      file.isDirectory() && recursive 
+        && openDirStream(`${file.path}/${file.name}`)
+          .pipeTo(new WriteableStream(write(file){controller,enqueue(`${file.path}/${file.name}`)}))
+        : controller.enqueue(`${file.path}/${file.name}`);
+    };
+  },
 })
 
-const IFileSystemHandle = (dirEnt,resolvedPath) => dirEnt ? ({
-  ...([,'file','directory','softLink','hardLink'].find((kind,i) => i === dirEnt[dirEnt[1]]) === 'directory' && IDirectoryHandle(dirEnt[0],resolvedPath))
-}) : IDirectoryHandle('.')
 
-export StorageManager = navigator ? navigator.storage : IDirectoryHandle('.')
+// 1=file, 2=directory,3=symLink
+// const type = Symbol.for("type");
+const kindSymbol = Symbol("kind");
+
+// dir.ent withFileTypes result of readdir(,{withFileType})
+// Reflect.ownKeys(Filehandle)[1] === Symbol.for("type"); FileHandle[type] = 1; 
+export const IFileSystemHandle = (name,kind,data) => {
+  
+  const handle = {
+    getDirectory: kind === 'directory' && () => fs.readdirSync('./',{recursive:true,withFileTypes: true})
+      .map((f)=>[f.name,f[Reflect.ownKeys(f)[1]]]),
+    getFile: kind === 'file' && () => data || Readable.toWeb(fs.createReadStream(
+      'data.txt', {encoding: 'utf-8'}))
+  }
+  
+  return ();
+}
 
 
-// Structure Advice
-// .versions/objects/blob // can be low level used without the tree but should update eventual tree if it exists. Dragons are here
-// .versions/objects/tree
-// .versions/objects/commit (Collection of tree and blob objects signed and verifyed)
-// .versions/refs/tags/versiontag => value is a commit ref
-// .versions/refs/metatags/name => returns commits. 
+export StorageManager = navigator ? navigator.storage : ({
+  getDirectory: () => ['name',dataOrArrayOfFileSystemHandle]
+});
+   
+export const handelsByPathname = {} 
 
+const getDirectoryHandle = async (pathname) => await pathname.split('/').reduce(
+    async (dir,dirName) => (dir = await dir.getDirectoryHandle(dirName,options)),
+    await navigator.storage.getDirectory()
+);
+
+export const readdir = async (pathname,options={create: false, recursive: false}) => !options.create && options.recursive 
+  ? readdirRecursiv(pathname) : (await getDirectoryHandle(pathname)).values;
+
+const resolvePathnameFromHandle = async (handle) => (await (await navigator.storage.getDirectory())
+  .resolve(handle)).join('/') + `/${handle.name}`
+
+export const readdirRecursiv = async (pathname) => (await readdir(pathname,{create:false})).flatMap(
+  handle => handle.kind === 'directory' 
+    ? [pathname, await readdirRecursiv(`${pathname}/${handle.name}`)]
+    : handle
+  )
+);
+
+const getDirPathnamesRecursiv = async (pathname) => (await readdir(pathname,{create:false}))
+  .filter((handel) => handle.kind === 'directory')
+  .map((dirHandle) => getDirPathnamesRecursiv(`${pathname}/${(await dirHandle).name}`));
+
+// Can returns exist fileHandel but can also create a fileHandle and path
+export const getFileHandleFrom = async (pathname,options={create:false}) => {
+  const [directoryName,fileName] = parsePath(pathname);
+  return await (await readdir(directoryName),options))
+    .getFileHandle(fileName),options);
+};
+
+// (pathname === '.' ||  pathname === '/' || pathname === './' )
+// Can return a directoryHandleEntrie even if a filePath is given it returns the directoryHandle of the filePath
+export const getDirectoryHandleEntrie = async (pathname = '.', options={create: false}) => 
+  await pathname.split('/').reduce(
+    async (dir,dirName) => (dir = dir.getDirectoryHandle 
+      ? [`${dir[0]}/${dirName}`, await dir[1].getDirectoryHandle(dirName, options)]
+      : [dir[0], await dir[1]])],
+    await navigator.storage.getDirectory()
+  );
+
+const getFileEntrie = async (pathname,handle) => {
+      const { name, kind } = handle;    
+      const { size, type, lastModified } = await handle.getFile();
+      return {
+          pathname,  
+          name, kind,
+          size, type, lastModified,
+      };
+};
+
+export const getDirectory = (baseUrl = '.') => new ReadableStream({ 
+  async start(stream){
+    const getDirectoryHandleEntrieRecursive = async (baseUrl = '.') => {
+      const [pathname, handle] = getDirectoryHandleEntrie(baseUrl,{create:false});
+      handelsByPathname[pathname] = await handle;
+      const { name, kind } = handle;
+      
+      const getEntrie = {
+        async file() {          
+          const entrie = await getFileEntrie(handle);
+          stream.enqueue([pathname, entrie]);
+          return [pathname,entrie];
+        },
+        async directory() {
+          return [name, {
+              pathname, name, kind,
+              entries: Object.fromEntries(await Promise.all((await handle.values()).map(async (nextHandle)=>
+                await getDirectoryHandleEntrieRecursive(`${pathname}/${(await nextHandle).name}`, await nextHandle)()
+              ))),
+          }];    
+        }
+      };
+      
+      return getEntrie[handle.kind]();
+    }
+    
+    getDirectoryHandleEntrieRecursive(baseUrl);
+  }
+})
+
+
+export const getRelativePath = async (handle) => `./${(await root.resolve(handle)).join('/')}`;    
+
+// [directoryName,fileName];
+const parsePath = (pathname) => [
+  pathname.slice(0,pathname.lastIndexOf('/'),
+  pathname.slice(pathname.lastIndexOf('/')+1
+];
+
+
+export const mkdirP = (pathname) => readdir(pathname,{create:true});
+
+export const onrequest = async (request, sender, sendResponse) => {
+  if (request.message?.startsWith('getDirectory')) {
+    return getDirectoryEntriesRecursive(request.data)
+        .pipeTo(new WriteableStream({ write(entrie)}){ 
+          sendResponse({ entrie });
+        });
+  } 
+  
+  if (request.message?.startsWith('save') === 'saveFile') {
+    const fileHandle = handelsByPathname[request.data];
+    try {
+      return (await fileHandle.getFile()).stream()
+        .pipeTo(await (await showSaveFilePicker({
+            suggestedName: fileHandle.name
+         })).createWritable());
+    } catch ({name, message}) {
+      name !== 'AbortError' &&
+        console.error(name, message);
+    }
+  }
+  
+  if (request.message?.startsWith('delete')) {
+    try {
+      await handelsByPathname[request.data].remove();
+      return sendResponse({ result: 'ok' });
+    } catch ({name, message}) {
+      console.error(name, message);
+      sendResponse({ error: message });
+    }
+  }
+};
